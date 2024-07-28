8bf45edfe0f3c8324e32677de43f613e24d3de2c
brown-cookies <jesimielalmedejar1018@gmail.com>
2024-07-28 16:43:37 +0800

    feat: change textarea to quilljs

--- professor/forms.py
+++ professor/forms.py
@@ -8,8 +8,10 @@ class UpdateCourseAboutForm(forms.ModelForm):
         widget=forms.Textarea(
             attrs={
                 "type": "text",
-                "class": "form-control",
+                "class": "form-control d-none",
                 "placeholder": "Update your 'About the Course' here...",
+                # TODO: Remove value after testing
+                "value": "Test Value",
                 "required": True,
             }
         ),
