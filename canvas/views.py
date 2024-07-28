a92a797af5f28ad8d9bf6a898e41edd488eda9b0
brown-cookies <jesimielalmedejar1018@gmail.com>
2024-07-28 09:29:15 +0800

    feat: add get_professor_courses for profile and security page

--- canvas/views.py
+++ canvas/views.py
@@ -6,93 +6,10 @@ from .forms import UserLoginForm
 from main.forms import ProfileForm, ChangePasswordForm
 from django.contrib import messages
 from django.contrib.auth import update_session_auth_hash
+from common.util.get_courses import get_professor_courses
 
 
 # Create your views here.
-courses = [
-    {
-        "name": "Data Analysis",
-        "course_code": "4714",
-        "codename": "DATANALYSIS",
-        "sy": "2024-2025",
-        "semester": "1st",
-        "link": "https://example.com",
-    },
-    {
-        "name": "Differentials",
-        "course_code": "4396",
-        "codename": "DIFFEQUATIONS",
-        "sy": "2024-2025",
-        "semester": "1st",
-        "link": "https://example.com",
-    },
-    {
-        "name": "Engineering Economics",
-        "course_code": "4399",
-        "codename": "ENGGECON",
-        "sy": "2024-2025",
-        "semester": "1st",
-        "link": "https://example.com",
-    },
-    {
-        "name": "Ethics",
-        "course_code": "4394",
-        "codename": "4ETHICS",
-        "sy": "2024-2025",
-        "semester": "1st",
-        "link": "https://example.com",
-    },
-    {
-        "name": "Fundamental of Electrical Circuit",
-        "course_code": "4400",
-        "codename": "ELECIRCUIT",
-        "sy": "2024-2025",
-        "semester": "1st",
-        "link": "https://example.com",
-    },
-    {
-        "name": "Fundamental of Electrical Circuit Laboratory",
-        "course_code": "4401",
-        "codename": "ELECIRCUITL",
-        "sy": "2024-2025",
-        "semester": "1st",
-        "link": "https://example.com",
-    },
-    {
-        "name": "Switching and Routing Essentials",
-        "course_code": "4403",
-        "codename": "INCOMTECH1",
-        "sy": "2024-2025",
-        "semester": "1st",
-        "link": "https://example.com",
-    },
-    {
-        "name": "Life and Works of Rizal",
-        "course_code": "4395",
-        "codename": "4RIZAL",
-        "sy": "2024-2025",
-        "semester": "1st",
-        "link": "https://example.com",
-    },
-    {
-        "name": "Objective-Oriented Programming",
-        "course_code": "4402",
-        "codename": "OBJORPROG",
-        "sy": "2024-2025",
-        "semester": "1st",
-        "link": "https://example.com",
-    },
-    {
-        "name": "Physical Education 3",
-        "course_code": "9466",
-        "codename": "7TPE3",
-        "sy": "2024-2025",
-        "semester": "1st",
-        "link": "https://example.com",
-    },
-]
-
-
 class HomePageView(View):
     def get(self, request, *args, **kwargs):
         return redirect("login")
@@ -138,6 +55,11 @@ class ProfilePageView(LoginRequiredMixin, View):
     def get(self, request, *args, **kwargs):
         form = ProfileForm()
 
+        user = self.request.user
+
+        # TODO: Add get_student_courses for student viewing profile page
+        courses = get_professor_courses(user)
+
         context = {
             "title": "Profile",
             "link": "profile",
@@ -156,6 +78,11 @@ class SecurityPageView(LoginRequiredMixin, View):
     def get(self, request, *args, **kwargs):
         form = ChangePasswordForm(self.request.user)
 
+        user = self.request.user
+
+        # TODO: Add get_student_courses for student viewing profile page
+        courses = get_professor_courses(user)
+
         context = {
             "title": "Security",
             "link": "security",
@@ -171,6 +98,11 @@ class SecurityPageView(LoginRequiredMixin, View):
 
     def post(self, request, *args, **kwargs):
         form = ChangePasswordForm(self.request.user, data=request.POST)
+
+        user = self.request.user
+
+        # TODO: Add get_student_courses for student viewing profile page
+        courses = get_professor_courses(user)
 
         context = {
             "title": "Security",
