547a111d4fd245b838d1861cc78bdaab9cb657bd
brown-cookies <jesimielalmedejar1018@gmail.com>
2024-08-03 13:36:04 +0800

    feat: migrate professor syllabus to dashboard syllabus

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views.views import Dashboard
 from .views.home import Home
+from .views.syllabus import Syllabus
 
 
 app_name = "dashboard"
@@ -8,7 +9,7 @@ app_name = "dashboard"
 urlpatterns = [
     path("", Dashboard.as_view(), name="dashboard"),
     path("<int:course_id>/home/", Home.as_view(), name="course-home"),
-    path("<int:course_id>/syllabus/", Dashboard.as_view(), name="course-syllabus"),
+    path("<int:course_id>/syllabus/", Syllabus.as_view(), name="course-syllabus"),
     path(
         "<int:course_id>/announcements/",
         Dashboard.as_view(),
---
+++ dashboard/views/syllabus.py
@@ -0,0 +1,29 @@
+from dashboard.views.views import View
+from django.views.generic import TemplateView
+from django.shortcuts import render
+from dashboard.models import Course
+
+
+class Syllabus(View, TemplateView):
+    template_name = "dashboard/syllabus/detail.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+
+        course_id = kwargs.pop("course_id", None)
+
+        course = Course.objects.get(pk=course_id)
+
+        context["title"] = f"{course.title} Syllabus"
+        context["link"] = "course"
+        context["sub_link"] = "syllabus"
+
+        context["course"] = course
+
+        return context
+
+    def get(self, request, *args, **kwargs):
+
+        context = self.get_context_data(*args, **kwargs)
+
+        return render(request, self.template_name, context)
