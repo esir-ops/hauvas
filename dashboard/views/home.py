6ac847b0d369d54dcd75cdfc69b6c06065b86797
brown-cookies <jesimielalmedejar1018@gmail.com>
2024-08-03 13:28:17 +0800

    feat: migrate professor app to dashboard app

--- dashboard/views/home.py
+++ dashboard/views/home.py
@@ -14,6 +14,7 @@ class Home(View, TemplateView):
 
         course = Course.objects.get(pk=course_id)
 
+        context["title"] = f"{course.title} Course"
         context["link"] = "course"
         context["sub_link"] = "home"
 
--- dashboard/views/views.py
+++ dashboard/views/views.py
@@ -21,5 +21,15 @@ class View(LoginRequiredMixin):
 class Dashboard(View, TemplateView):
     template_name = "dashboard/index.html"
 
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+
+        context["link"] = "dashboard"
+
+        return context
+
     def get(self, request, *args, **kwargs):
-        return render(request, self.template_name, self.get_context_data())
+
+        context = self.get_context_data(*args, **kwargs)
+
+        return render(request, self.template_name, context)
