78e73ea6c9431ae5c2e1e68f8bc79f0feef30d19
brown-cookies <jesimielalmedejar1018@gmail.com>
2024-08-03 13:00:37 +0800

    feat: add course url path

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,6 +1,23 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 app_name = "dashboard"
 
-urlpatterns = [path("", views.Dashboard.as_view(), name="dashboard")]
+urlpatterns = [
+    path("", views.Dashboard.as_view(), name="dashboard"),
+    path("<int:course_id>/home/", views.Dashboard.as_view(), name="course-home"),
+    path(
+        "<int:course_id>/syllabus/", views.Dashboard.as_view(), name="course-syllabus"
+    ),
+    path(
+        "<int:course_id>/announcements/",
+        views.Dashboard.as_view(),
+        name="course-announcement",
+    ),
+    path("<int:course_id>/modules/", views.Dashboard.as_view(), name="course-module"),
+    path(
+        "<int:course_id>/assignments/",
+        views.Dashboard.as_view(),
+        name="course-assignment",
+    ),
+]
