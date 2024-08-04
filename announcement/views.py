2822135b3a152b384976f731d53c696fcad448f7
brown-cookies <jesimielalmedejar1018@gmail.com>
2024-08-04 13:27:05 +0800

    feat: add create, detail, and update template for samia to design

--- announcement/views.py
+++ announcement/views.py
@@ -20,3 +20,57 @@ class AnnouncementList(View, TemplateView):
         context = self.get_context_data(*args, **kwargs)
 
         return render(request, self.template_name, context)
+
+
+class AnnouncementDetail(View, TemplateView):
+    template_name = "announcement/detail.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+
+        context["title"] = "Announcements"
+        context["link"] = "announcement"
+
+        return context
+
+    def get(self, request, *args, **kwargs):
+
+        context = self.get_context_data(*args, **kwargs)
+
+        return render(request, self.template_name, context)
+
+
+class AnnouncementCreate(View, TemplateView):
+    template_name = "announcement/create.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+
+        context["title"] = "Announcements"
+        context["link"] = "announcement"
+
+        return context
+
+    def get(self, request, *args, **kwargs):
+
+        context = self.get_context_data(*args, **kwargs)
+
+        return render(request, self.template_name, context)
+
+
+class AnnouncementUpdate(View, TemplateView):
+    template_name = "announcement/update.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+
+        context["title"] = "Announcements"
+        context["link"] = "announcement"
+
+        return context
+
+    def get(self, request, *args, **kwargs):
+
+        context = self.get_context_data(*args, **kwargs)
+
+        return render(request, self.template_name, context)
