5e0fb52c9e33d53ec443b4faff42ba54a8fe16fb
brown-cookies <jesimielalmedejar1018@gmail.com>
2024-08-04 13:31:59 +0800

    feat: add create, detail, and update template for chin to design

--- grade/urls.py
+++ grade/urls.py
@@ -3,4 +3,14 @@ from . import views
 
 app_name = "grade"
 
-urlpatterns = [path("", views.GradeList.as_view(), name="grade")]
+# URL PATTERN <int:grade_id>
+# Example: <int:grade_id>/detail/ <int:grade_id>/create/
+
+# TODO: Add dynamic url after Chin Finish designing
+
+urlpatterns = [
+    path("", views.GradeList.as_view(), name="grade-list"),
+    path("detail/", views.GradeDetail.as_view(), name="grade-detail"),
+    path("create/", views.GradeCreate.as_view(), name="grade-create"),
+    path("update/", views.GradeUpdate.as_view(), name="grade-update"),
+]
--- grade/views.py
+++ grade/views.py
@@ -20,3 +20,57 @@ class GradeList(View, TemplateView):
         context = self.get_context_data(*args, **kwargs)
 
         return render(request, self.template_name, context)
+
+
+class GradeDetail(View, TemplateView):
+    template_name = "grade/detail.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+
+        context["title"] = "Grades"
+        context["link"] = "grade"
+
+        return context
+
+    def get(self, request, *args, **kwargs):
+
+        context = self.get_context_data(*args, **kwargs)
+
+        return render(request, self.template_name, context)
+
+
+class GradeCreate(View, TemplateView):
+    template_name = "grade/create.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+
+        context["title"] = "Grades"
+        context["link"] = "grade"
+
+        return context
+
+    def get(self, request, *args, **kwargs):
+
+        context = self.get_context_data(*args, **kwargs)
+
+        return render(request, self.template_name, context)
+
+
+class GradeUpdate(View, TemplateView):
+    template_name = "grade/update.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+
+        context["title"] = "Grades"
+        context["link"] = "grade"
+
+        return context
+
+    def get(self, request, *args, **kwargs):
+
+        context = self.get_context_data(*args, **kwargs)
+
+        return render(request, self.template_name, context)
