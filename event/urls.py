f425dca968424b136b2cc803555dd0f3bbadca54
brown-cookies <jesimielalmedejar1018@gmail.com>
2024-08-04 13:36:18 +0800

    feat: add create, detail, and update template for sicat to design

--- event/urls.py
+++ event/urls.py
@@ -3,4 +3,14 @@ from . import views
 
 app_name = "event"
 
-urlpatterns = [path("", views.EventList.as_view(), name="event")]
+# URL PATTERN <int:event_id>
+# Example: <int:event_id>/detail/ <int:event_id>/create/
+
+# TODO: Add dynamic url after Sicat Finish designing
+
+urlpatterns = [
+    path("", views.EventList.as_view(), name="event-list"),
+    path("detail/", views.EventDetail.as_view(), name="event-detail"),
+    path("create/", views.EventCreate.as_view(), name="event-create"),
+    path("update/", views.EventUpdate.as_view(), name="event-update"),
+]
--- event/views.py
+++ event/views.py
@@ -20,3 +20,57 @@ class EventList(View, TemplateView):
         context = self.get_context_data(*args, **kwargs)
 
         return render(request, self.template_name, context)
+
+
+class EventDetail(View, TemplateView):
+    template_name = "event/detail.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+
+        context["title"] = "Events"
+        context["link"] = "event"
+
+        return context
+
+    def get(self, request, *args, **kwargs):
+
+        context = self.get_context_data(*args, **kwargs)
+
+        return render(request, self.template_name, context)
+
+
+class EventCreate(View, TemplateView):
+    template_name = "event/create.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+
+        context["title"] = "Events"
+        context["link"] = "event"
+
+        return context
+
+    def get(self, request, *args, **kwargs):
+
+        context = self.get_context_data(*args, **kwargs)
+
+        return render(request, self.template_name, context)
+
+
+class EventUpdate(View, TemplateView):
+    template_name = "event/update.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+
+        context["title"] = "Events"
+        context["link"] = "event"
+
+        return context
+
+    def get(self, request, *args, **kwargs):
+
+        context = self.get_context_data(*args, **kwargs)
+
+        return render(request, self.template_name, context)
