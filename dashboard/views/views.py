5ab7735ea84af92d5656dece33afdda05ce76b5f
brown-cookies <jesimielalmedejar1018@gmail.com>
2024-08-16 18:28:18 +0800

    feat: make the allowed_roles available globally in the dashboard

--- dashboard/views/views.py
+++ dashboard/views/views.py
@@ -1,15 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from django.utils.decorators import method_decorator
 
-from common.util.group import UserGroup
 from common.util.views import View
-from common.util.decorators import allowed_roles
 
 
-@method_decorator(
-    allowed_roles(roles=[UserGroup.PROFESSOR, UserGroup.STUDENT]), name="dispatch"
-)
 class Dashboard(View, TemplateView):
     template_name = "dashboard/index.html"
 
