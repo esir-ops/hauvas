4472c1c737df5dbb1fa076b7863e9f36c357ee24
brown-cookies <jesimielalmedejar1018@gmail.com>
2024-08-21 06:28:37 +0800

    feat: add permission required

--- dashboard/util/view.py
+++ dashboard/util/view.py
@@ -1,6 +1,7 @@
 from django.http import Http404
 from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView
+from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.shortcuts import get_object_or_404
 
 from dashboard.models import Course
@@ -9,7 +10,11 @@ from dashboard.decorator.validation import role_have_course_access
 
 
 @method_decorator(role_have_course_access, name="dispatch")
-class DashboardParentView(View, TemplateView):
+class DashboardParentView(PermissionRequiredMixin, View, TemplateView):
+
+    permission_required = ["dashboard.view_course"]
+    permission_denied_message = "You're not allowed to view this course!"
+
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
 
